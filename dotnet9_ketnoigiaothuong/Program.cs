8eb762f6919c5f42485e3af0605c0b24bc00438c
Duc Hien <2001215763@hufi.edu.vn>
2025-04-30 22:50:58 +0700

    feature_hien/remove_comment

--- dotnet9_ketnoigiaothuong/Program.cs
+++ dotnet9_ketnoigiaothuong/Program.cs
@@ -2,10 +2,7 @@
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddEndpointsApiExplorer();
@@ -24,7 +21,6 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
